c8c1e8c21eac7132b7fdca178f3e31a091385bd8
Fernando Mazola <fernando_mazola@hotmail.com>
2017-08-15 08:32:06 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { PizzariaComponent } from './pizzaria/pizzaria.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PizzariaComponent,
+    ProdutosComponent,
+    SobreComponent
   ],
   imports: [
     BrowserModule
